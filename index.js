c7149db505bca86db79db8af1a82596053979468
Your Namewhtft <whatafairfooot@gmail.com>
2020-04-21 10:31:50 +0200

    I hate you socket.io

--- index.js
+++ index.js
@@ -1,30 +1,35 @@
 const express = require("express");
 const primitive = require("primitive");
 const http = require("http");
+const Primus = require('primus')
 
 const app = express();
 const server = http.createServer(app);
-const io = require("socket.io")(server);
+const primus = new Primus(server);
 
 app.use(express.json({limit:"10mb", extended: true}));
 app.use("/", express.static(__dirname+"/www/"));
 
+let steps, step;
 app.post("/primitive", async (req,res)=>{
   if(!req.body.image) return res.sendStatus(400);
   res.sendStatus(200);
-  const steps = req.body.steps || 10;
-  const primed = await primitive({
-    input: req.body.image
-    ,numSteps: steps
-    ,onStep: onStepCallback
+  steps = step = req.body.steps || 10;
+  primitive({
+    input: req.body.image,
+    numSteps: steps,
+    onStep: onStepCallback
+  }).then(primed=>{
+    return primed.toSVG()
+  }).then(svg=>{
+    primus.write({event: "result", data: svg});
   });
-  const svg = await primed.toSVG();
-  io.emit("result", svg);
 });
 
-const onStepCallback = foo=>{
-  console.log(foo.score);
-  io.emit("progress", foo.score);
+const onStepCallback = ()=>{
+  step--;
+  const progress = (1 - step / steps) * 100;
+  primus.write({event: "step", data: progress});
 }
 
 server.listen(7777);
